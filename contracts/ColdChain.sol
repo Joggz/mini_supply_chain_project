8c671df2426238c7bd62c2f15c0c0562fe18a08a
laoye_jnr <idrisaboladeo@gmail.com>
2022-02-07 22:15:44 +0100

    :beers: add need enum, structs and mapping

--- contracts/ColdChain.sol
+++ contracts/ColdChain.sol
@@ -17,6 +17,45 @@ library CryptoSuite {
     function recoverSigner(bytes2 message, bytes memory sig) internal pure returns (address) {
         (uint8 v, bytes32 r, bytes32 s) = splitSignature(sig);
 
-        return erecover(message, v, r, s);
+        return ecrecover(message, v, r, s);
     }
+}
+
+contract ColdChain {
+    enum CertificateStatus { MANFACTURED, STORED, DELIVERED,DELIVERED_LOCAL, DELIVERED_INTERNATIONAL, VACCINATED }
+    struct Certificate {
+        uint id;
+        Entity issuer;
+        Entity prover;
+        bytes signature;
+        Status status;
+    }
+
+    enum Mode { ISSUER, PROVER, VERIFIER}
+    struct Entity {
+        address id;
+        Mode mode;
+        uint[] certificateIds;
+        Status status;
+    }
+    struct VaccinateBatch {
+        uint id;
+        string brand;
+        address manufacturer;
+        uint[] certificateIds;
+    }
+    uint public constant MAX_CERTIFICATIONs = 2;
+    uint[] public certificateIds;
+    uint[] public vaccineBatchIds;
+
+    mapping (uint => VaccineBatch) public vaccineBatches;
+    mapping(uint => Certificate) public certificates;
+      mapping(address => Entity) public entities;
+
+
+    event AddEntity(address entityId, string entityMode);
+        event AddVaccineBatch(uint vaccineBatchId, address indexed manufacturer);
+    event IssueCertificate(address indexed issuer, address prover, uint certificateId);
+
+
 }
